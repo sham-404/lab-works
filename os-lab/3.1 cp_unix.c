0ed1bcd362237ad4c59b568e3ae7dfd7f6a559aa
sham-404 <shamsujith4@gmail.com>
2026-01-20 22:33:51 +0530

    added 3.1, not sure is it working tho

---
+++ os-lab/3.1 cp_unix.c
@@ -0,0 +1,23 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char ch;
+    int sc = 0;
+    fp = fopen(argv[1], "r");
+
+    if (fp == NULL)
+        printf("unable to open a file %s", argv[1]);
+    else {
+        while (!feof(fp)) {
+            ch = fgetc(fp);
+            if (ch == ' ')
+                sc++;
+        }
+        printf("no of spaces %d", sc);
+        printf("\n");
+        fclose(fp);
+    }
+}
