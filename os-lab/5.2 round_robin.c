206ba162006ebb6720a2c6062e54c4c4ec569648
sham-404 <shamsujith4@gmail.com>
2026-01-26 20:45:09 +0530

    Added 5.1 and 5.2

---
+++ os-lab/5.2 round_robin.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct rr {
+    int pno, btime, sbtime, wtime, lst;
+} p[10];
+
+int main() {
+    int pp = -1, ts, flag, count, ptm = 0, i, n, twt = 0, totttime = 0;
+    printf("\n round robin scheduling............");
+    printf("Enter no of processes:");
+    scanf("%d", &n);
+    printf("Enter the time slice:");
+    scanf("%d", &ts);
+    printf("Enter the burst time");
+
+    for (i = 0; i < n; i++) {
+        printf("\n process%d\t", i + 1);
+        scanf("%d", &p[i].btime);
+        p[i].wtime = p[i].lst = 0;
+        p[i].pno = i + 1;
+        p[i].sbtime = p[i].btime;
+    }
+
+    printf("scheduling....\n");
+    do {
+        flag = 0;
+        for (i = 0; i < n; i++) {
+            count = p[i].btime;
+            if (count > 0) {
+                flag = -1;
+                count = (count >= ts) ? ts : count;
+                printf("\n process %d", p[i].pno);
+                printf("from%d", ptm);
+                ptm += count;
+                printf("to%d", ptm);
+                p[i].btime -= count;
+                if (pp != i) {
+                    pp = i;
+                    p[i].wtime += ptm - p[i].lst - count;
+                    p[i].lst = ptm;
+                }
+            }
+        }
+    } while (flag == -1);
+}
